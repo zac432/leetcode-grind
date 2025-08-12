8d0826cbb2d9bb87929119de69cb2bed3d753cf7
zac432 <zel@torontomu.ca>
2025-08-12 02:48:41 -0400

    added the solution for 003

--- 003_palindrome_number/solution.py
+++ 003_palindrome_number/solution.py
@@ -1,3 +1,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        
+     s = str(x) # converted input to string 
+     if s == s[::-1]: #reversed string and checked if its equal so  if 121 is reversed it will be 121
+        return True 
+     else:
+        return False
